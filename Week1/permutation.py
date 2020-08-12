f88e68c301bd7f30390a21ccf5be2d71bde9d54a
Tanapol Wong-asa <korn_tanapol@outlook.co.th>
2020-08-12 23:44:41 +0700

    Permutation but in wrong order

--- Week1/permutation.py
+++ Week1/permutation.py
@@ -1,9 +1,40 @@
-# @todo #1:2days Fix permutation order, Check permutation code
+# @todo #2:1days Fix permutation order
+def permutation(lst):
+    # If lst is empty then there are no permutations
+    if len(lst) == 0:
+        return []
+
+        # If there is only one element in lst then, only
+    # one permuatation is possible
+    if len(lst) == 1:
+        return [lst]
+
+        # Find the permutations for lst if there are
+    # more than 1 characters
+
+    l = []  # empty list that will store current permutation
+
+    # Iterate the input(lst) and calculate the permutation
+    for i in range(len(lst)):
+        m = lst[i]
+
+        # Extract lst[i] or m from the list.  remLst is
+        # remaining list
+        remLst = lst[:i] + lst[i + 1:]
+
+        # Generating all permutations where m is first
+        # element
+        for p in permutation(remLst):
+            l.append([m] + p)
+    return l
+
+
 if __name__ == '__main__':
     print('*** Fun with permute ***')
     num = [int(i) for i in input('input : ').split(',')]
-    ans = []
+    # ans = []
     print('Original Cofllection: ', num)  # Typo for grader purpose
+    """
     # ans.append(num)
     num.reverse()
     for i in range(len(num)):
@@ -25,5 +56,8 @@ if __name__ == '__main__':
     rev = num.copy()
     rev.reverse()
     # ans.append(rev)
+    """
+    ans = permutation(num)
+    ans.reverse()
     print('Collection of distinct numbers:\n', ans)
 
