d2d37371cff9c0af7d0f9b8054429b6cbf0a3c3e
Tanapol Wong-asa <korn_tanapol@outlook.co.th>
2020-11-07 01:26:48 +0700

    Week9 - Insertion Sort with Recursion

---
+++ Week9/insertion_rec.py
@@ -0,0 +1,34 @@
+def insertion_index(lst, curr_index, value):  # shift list value and return insertion index
+    if curr_index > 0 and lst[curr_index - 1] > value:
+        lst[curr_index] = lst[curr_index - 1]
+        return insertion_index(lst, curr_index - 1, value)
+    else:
+        return curr_index
+
+
+def insertion_sort(lst, start=0, length=None, progress=0):
+    if length is None:  # for less parameters passing
+        length = len(lst)
+
+    value = lst[start]  # value to be inserted
+
+    index = insertion_index(lst, start, value)  # get insertion index and shift the list
+    lst[index] = value  # insert value to the index
+    progress += 1  # increase sorted part length
+
+    if progress > 1:  # print insertion process (skip the first time)
+        if len(lst[progress:]) != 0:  # prevent empty list printing '[]'
+            print(f"insert {value} at index {index} : {lst[:progress]} {lst[progress:]}")
+        else:
+            print(f"insert {value} at index {index} : {lst[:progress]}")
+
+    if start+1 < length:  # do until the last element
+        insertion_sort(lst, start+1, length, progress)  # move to insert next element
+
+
+if __name__ == '__main__':
+    in_lst = list(map(int, input("Enter Input : ").split()))
+    out = []
+    insertion_sort(in_lst)
+    print("sorted")
+    print(in_lst)
