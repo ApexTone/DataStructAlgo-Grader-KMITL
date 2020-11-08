c990233f6981618644153add6a4a77e04c972a8b
Tanapol Wong-asa <korn_tanapol@outlook.co.th>
2020-11-08 16:37:33 +0700

    Week9 - selection_rec compatibility with grader

--- Week9/selection_rec.py
+++ Week9/selection_rec.py
@@ -1,20 +1,26 @@
-def selection_sort_rec(lst, left=0, sorted_lst=None):
-    if sorted_lst is None:
-        sorted_lst = []
-    if left >= len(lst):
-        return sorted_lst
-    min_value = 999999999999
-    min_index = -1
-    for i in range(left, len(lst)):
-        if min_value > lst[i]:
-            min_value = lst[i]
-            min_index = i
-    lst[left], lst[min_index] = lst[min_index], lst[left]
-    sorted_lst.append(lst[left])
-    return selection_sort_rec(lst, left+1, sorted_lst)
+def get_max_index(lst):
+    right = len(lst)-1
+
+    if lst[right] != max(lst) and right-1 >= 0:
+        return get_max_index(lst[:right])
+    else:
+        return right
+
+
+def selection_sort_rec(lst, right=None):
+    if right is None:
+        right = len(lst)-1
+    if right < 0:
+        return lst
+
+    max_index = get_max_index(lst[:right+1])
+    if max_index != right:  # if not in correct position
+        lst[right], lst[max_index] = lst[max_index], lst[right]
+        print(f'swap {lst[max_index]} <-> {lst[right]} : {lst}')
+    return selection_sort_rec(lst, right-1)
 
 
 if __name__ == '__main__':
     in_lst = list(map(int, input("Enter Input : ").split()))
     ans = selection_sort_rec(in_lst)
-    print(ans)
+    print(in_lst)
